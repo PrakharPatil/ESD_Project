d271e3c340fbff72db86df20b2173b7db18c3c5d
Prakhar Patil <prakhar_patil@Prakhars-MacBook-Air.local>
2024-11-16 17:35:44 +0530

    First Commit

---
+++ yummyapp/src/main/java/com/prakharpatil/yummyapp/YummyappApplication.java
@@ -0,0 +1,13 @@
+package com.prakharpatil.yummyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyappApplication.class, args);
+	}
+
+}
---
+++ yummyapp/src/test/java/com/prakharpatil/yummyapp/YummyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakharpatil.yummyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
